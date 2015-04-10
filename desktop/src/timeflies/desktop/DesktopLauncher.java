fb88d7e9116417912e8c1b61a4b148970eb93f92
Richard <richardhaddad@hotmail.fr>
2015-04-11 00:16:30 +0200

    Premier Commit

---
+++ desktop/src/timeflies/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package timeflies.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import timeflies.TimeFlies;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TimeFlies(), config);
+	}
+}
