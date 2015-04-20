fa47dba9d095472c7c6645f60dd43f8e0d7551a5
Alexandre Gaspard-Cilia <a.gaspardcilia@gmail.com>
2015-04-20 21:40:30 +0200

    Zones has been implemented (except Diagonales.java) but not tested.

--- core/src/gameplay/sort/zone/Carre.java
+++ core/src/gameplay/sort/zone/Carre.java
@@ -5,21 +5,54 @@
  */
 package gameplay.sort.zone;
 
+import gameplay.map.Map;
+import gameplay.map.Tuile;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
 /**
  * Carre.java
  * Représente une zone en forme de carré.
  *
  */
 public class Carre extends Zone {
-
+        
 	/**
 	 *
 	 * @param cote	longueur d'un coté
 	 * @param posit	 positif ?
 	 */
-	public Carre(int cote, boolean posit) {
-		super(posit);
+	public Carre(int cote, boolean posit, Map currentMap) {
+		super(posit, cote, currentMap);
 		//TODO cote
 	}
+
+    //Not tested yet.
+    @Override
+    public Tuile[] getTilesOfInterrest(Point center) {
+        ArrayList<Tuile> tiles = new ArrayList<Tuile>();
+        Tuile[][] mapTiles = currentMap.getTabTuiles();
+        
+        int maxX = currentMap.getMapDimension().width;
+        int maxY = currentMap.getMapDimension().height;
+        
+        Point start = new Point(super.valueInBound(center.x - size, maxX), super.valueInBound(center.y - size, maxY));
+        Point stop = new Point(super.valueInBound(center.x + size, maxX), super.valueInBound(center.y + size, maxY));
+       
+        
+        for (int x = start.x; x <= stop.x; x++) {
+            for (int y = start.y; y <= stop.y; y++) {
+                tiles.add(mapTiles[x][y]);
+            }
+        }
+        
+        Tuile[] result = new Tuile[tiles.size()]; 
+        return tiles.toArray(result);
+    }
+        
+
+    
+        
 
 }
